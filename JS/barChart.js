f2b7db5a39051a3a900ec9d2584d8c64061b1a45
Roman Fedoruk <rfedoruk21@gmail.com>
2017-01-25 19:05:54 -0500

    Changed the js a bit

--- JS/barChart.js
+++ JS/barChart.js
@@ -1,14 +1,18 @@
 $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(data){
     
-    d3.select('.chart')
-        .selectAll('g')
-        .data(data.data)
-        .enter()
-        .append('div')
-        .text(function(d){ return d[1];})
-        .attr('class', 'data')
-        .style('height', '10px')
-        .style('width', '10px');
+    var svg = d3.select(".chart").attr("width", 1200).atttr("height", 400)
+    
+//    d3.select('.chart')
+//        .selectAll('g')
+//        .data(data.data)
+//        .enter()
+//        .append('div')
+//        .text(function(d){ return d[1];})
+//        .attr('class', 'data')
+//        .style('height', '10px')
+//        .style('width', '10px');
+//    
+    svg.selectAll("rect").data(data.data).enter().append("rect").text(function(d){ return d[1];});
 	
 });
 
